036f2c6ced2aa95ede056124c9a5633c298fa960
Kyle Mercer <mercer_k@verizon.net>
2018-04-28 18:24:31 -0400

    GS: add training mode button and LED

--- projects/ground_station/gs_async_main/gs_async_main.cpp
+++ projects/ground_station/gs_async_main/gs_async_main.cpp
@@ -31,7 +31,10 @@ static SoftwareSerial BT_SERIAL_IF(BT_RX_PIN, BT_TX_PIN);
 #define CMD_BUFF_SIZE 32
 #define CS_PIN 9
 #define HUBSAN_TX_PERIOD_US 10000
+
 #define BIND_LED_PIN 2
+#define TRAINING_LED_PIN A0
+#define TRAINING_BUT_PIN A1
 
 static bt_smirf bt(BT_SERIAL_IF);
 static Q_Hubsan qh;
@@ -41,6 +44,7 @@ uint8_t cmd[CMD_BUFF_SIZE];
 q_hubsan_flight_controls_t fltCnt;
 q_status_msg_t status;
 unsigned long txTimestamp = 0;
+bool trainingEnabled = false;
 
 void hubsanControlUpdate() {
 
@@ -90,6 +94,59 @@ void initHubsanInterface(void) {
     digitalWrite(BIND_LED_PIN, HIGH);
 }
 
+void initTrainingFeature() {
+
+    pinMode(TRAINING_LED_PIN, OUTPUT);
+    pinMode(TRAINING_BUT_PIN, INPUT_PULLUP);
+
+    trainingEnabled = false;
+    digitalWrite(TRAINING_LED_PIN, LOW);
+}
+
+void handleTrainingButtonEvent() {
+
+    static int lastButtonState = HIGH;
+    static unsigned long lastDebounceTime = 0;
+    static const unsigned long debounceDelay = 50;
+
+    static int ledState = LOW;
+    static int buttonState = HIGH;
+
+    // read the state of the switch into a local variable:
+    int reading = digitalRead(TRAINING_BUT_PIN);
+    //digitalWrite(TRAINING_LED_PIN, reading);
+
+    // check to see if you just pressed the button
+    // (i.e. the input went from LOW to HIGH), and you've waited long enough
+    // since the last press to ignore any noise:
+
+    // If the switch changed, due to noise or pressing:
+    if (reading != lastButtonState) {
+        // reset the debouncing timer
+        lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+        // whatever the reading is at, it's been there for longer than the debounce
+        // delay, so take it as the actual current state:
+
+        // if the button state has changed:
+        if (reading != buttonState) {
+            buttonState = reading;
+
+            // only toggle the LED if the new button state is HIGH
+            if (buttonState == HIGH) {
+                ledState = !ledState;
+            }
+        }
+    }
+    // set the LED:
+    digitalWrite(TRAINING_LED_PIN, ledState);
+
+    // save the reading. Next time through the loop, it'll be the lastButtonState:
+    lastButtonState = reading;
+}
+
 void initTimer() {
 
     txTimestamp = micros();
@@ -105,6 +162,7 @@ void setup(void) {
     initSerialDebug();
     initBluetoothInterface();
     initHubsanInterface();
+    initTrainingFeature();
     initTimer();
 }
 
@@ -129,6 +187,8 @@ void loop(void) {
         }
         sendStatusResp();
     }
+
+    handleTrainingButtonEvent();
 
     /*
      * Wait 10ms since last update to send
